2427db9c85293fe3a370c461df31e6c6c782d179
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2022-12-06 07:36:39 +0200

    Day6

---
+++ day6.erl
@@ -0,0 +1,27 @@
+-module(day6).
+-compile(export_all).
+
+input()->
+    hd(fileutil:lines("day6.txt")).
+
+
+at_distinct(Count, Lst) ->
+    case sets:size(sets:from_list(lists:sublist(Lst, 1, Count))) of
+        Count -> true;
+        _ -> false
+    end.
+
+at_sop(Lst) -> at_distinct(4, Lst).
+at_som(Lst) -> at_distinct(14, Lst).
+
+marker_pos(I, MarkerFun, Lst) ->
+    case MarkerFun(Lst) of
+        true -> I;
+        _ -> marker_pos(I+1, MarkerFun, tl(Lst))
+    end.
+
+part1() ->
+    marker_pos(4, fun at_sop/1, input()).
+
+part2() ->
+    marker_pos(14, fun at_som/1, input()).
