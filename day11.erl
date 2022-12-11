1b5aced5e0f3071900e92505c89e00c1f0b85718
Tatu Tarvainen <tatu.tarvainen@me.com>
2022-12-11 16:40:45 +0200

    Use the same main loop for both parts
    Give the worry regulating function as parameter.

--- day11.erl
+++ day11.erl
@@ -42,20 +42,21 @@ arg(_, X) -> X.
 psyop(Old, {'*', Arg1, Arg2}) -> arg(Old,Arg1) * arg(Old,Arg2);
 psyop(Old, {'+', Arg1, Arg2}) -> arg(Old,Arg1) + arg(Old,Arg2).
 
-monkey_around(Monkeys) ->
-    lists:foldl(fun monkey_around/2, Monkeys, lists:seq(1, size(Monkeys))).
+monkey_around(Monkeys,WorryRegulator) ->
+    lists:foldl(fun(N,Ms)-> monkey_around(N,Ms,WorryRegulator) end,
+                Monkeys, lists:seq(1, size(Monkeys))).
 
 update(Monkeys, Idx, Fun) ->
     setelement(Idx, Monkeys, Fun(element(Idx,Monkeys))).
 
-monkey_around(N, Monkeys0) ->
+monkey_around(N, Monkeys0, WorryRegulator) ->
     %% Get the Nth monkey
     #monkey{items=Items, op=Op,
             test=Test,if_true=IfT, if_false=IfF} = element(N, Monkeys0),
     Monkeys1 =
         lists:foldl(
           fun(Item0,Monkeys) ->
-                  Item1 = floor(psyop(Item0, Op)/3),
+                  Item1 = WorryRegulator(psyop(Item0, Op)),
                   ThrowTo =
                       if (Item1 rem Test) == 0 -> IfT+1;
                          true -> IfF+1
@@ -76,37 +77,21 @@ monkey_business_level(Monkeys) ->
                    tuple_to_list(Monkeys)),
     Top1#monkey.inspections * Top2#monkey.inspections.
 
-part1() -> monkey_business_level(play(20,fun monkey_around/1, input())).
+part1() ->
+    Relieved = fun(W) -> floor(W/3) end,
+    monkey_business_level(
+      play(20, fun(Ms)->monkey_around(Ms,Relieved) end, input())).
 
 
 %%% part2
 
-monkey_around_unrelieved(Monkeys) ->
-    lists:foldl(fun monkey_around_unrelieved/2, Monkeys, lists:seq(1, size(Monkeys))).
-
 multiplier(Monkeys) ->
     lists:foldl(fun(#monkey{test=A},B)-> A * B end, 1, tuple_to_list(Monkeys)).
 
-monkey_around_unrelieved(N, Monkeys0) ->
-    %% Get the Nth monkey
-    #monkey{items=Items, op=Op,
-            test=Test,if_true=IfT, if_false=IfF} = element(N, Monkeys0),
-    Multiplier = multiplier(Monkeys0),
-    Monkeys1 =
-        lists:foldl(
-          fun(Item0,Monkeys) ->
-                  Item1 = psyop(Item0, Op) rem Multiplier,
-                  ThrowTo =
-                      if (Item1 rem Test) == 0 -> IfT+1;
-                         true -> IfF+1
-                      end,
-                  update(Monkeys, ThrowTo,
-                         fun(#monkey{items=It}=M) -> M#monkey{items=It++[Item1]} end)
-          end, Monkeys0, Items),
-    update(Monkeys1, N,
-           fun(#monkey{inspections=Ins}=Mo) ->
-                   Mo#monkey{items=[],inspections=Ins+length(Items)} end).
-
-
 part2() ->
-    monkey_business_level(play(10000, fun monkey_around_unrelieved/1, input())).
+    Monkeys = input(),
+    Mult = multiplier(Monkeys),
+    WorryRemainder = fun(W) -> W rem Mult end,
+    monkey_business_level(
+      play(10000,
+           fun(Ms) -> monkey_around(Ms, WorryRemainder) end, Monkeys)).
