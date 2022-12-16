61dd4190283b37ad6af625a060df640fd41d9927
Tatu Tarvainen <tatu.tarvainen@solita.fi>
2022-12-16 21:42:18 +0200

    Part2

--- day16.erl
+++ day16.erl
@@ -23,22 +23,14 @@ build_graph(Valves) ->
 
     G.
 
-%% total possible TPR is calculated from current time remaining, and the path to a valve and its rate
-possible_tpr(TimeRemaining, PathTo, #valve{rate=Rate}=V) ->
-    io:format("~p possible_tpr T=~p, Path=~p  => ~p~n", [V, TimeRemaining, PathTo, Rate * (TimeRemaining - length(PathTo) - 1)]),
-    io:format("  pressure mins ~p - ~p - 2 => ~p", [TimeRemaining, length(PathTo), (TimeRemaining - (length(PathTo) - 1) - 1)]),
-    Rate * (TimeRemaining - (length(PathTo) - 1) - 1).
-
-
-%% all permutations
-perms([]) -> [[]];
-perms(L)  -> [[H|T] || H <- L, T <- perms(L--[H])].
 
 %% Traveling salesman problem, take all possible permutations of the order, calculate how much it
 %% will relieve.
 
-%%-record(actor, {at="AA", path=[]}).
--record(route, {g, time=30, at="AA", path=[], nodes, tpr=0}).
+-record(actor, {at="AA", path=[]}).
+-record(route, {g, time=30, tpr=0, nodes,
+                elf=#actor{},
+                elephant=undefined}).
 
 pressure(G) ->
     lists:sum([ case digraph:vertex(G, V) of
@@ -47,46 +39,55 @@ pressure(G) ->
                 end ||
                   V <- digraph:vertices(G) ]).
 
-route(#route{time=0, tpr=Tpr}) -> Tpr; % time is up, just return the TPR
-route(#route{g=G, at=At, path=[], time=T, nodes=Nodes, tpr=Tpr}=R) ->
-    NextTpr = Tpr + pressure(G),
-    %% Exhausted a path, open the current node
+update_actor(_, undefined, Nodes) -> {undefined,Nodes};
+update_actor(G, #actor{at=At, path=[]}, Nodes) ->
+    %% Path exhausted, open this valve and select a new path
     {_,OldV} = digraph:vertex(G, At),
     digraph:add_vertex(G, At, OldV#valve{open = true}),
     %% Calculate next path step route to next
     case Nodes of
-        [] -> route(R#route{time=T-1, tpr=NextTpr});
+        [] -> {undefined, []};
         [N|Rest] ->
             %% Calculate path to next target valve
             [_|Path] = digraph:get_short_path(G, At, N),
-            route(R#route{at=At, time=T-1, path=Path, nodes=Rest, tpr=NextTpr})
+            {#actor{at=At, path=Path}, Rest}
     end;
-route(#route{g=G, path=[To|Path], time=T, tpr=Tpr}=R) ->
-    %% Going to the next valve
+update_actor(_, #actor{path=[To|Path]}, Nodes) ->
+    %% Traveling to target
+    {#actor{at=To, path=Path}, Nodes}.
+
+route(#route{time=0, tpr=Tpr}) -> Tpr; % time is up, just return the TPR
+route(#route{g=G, elf=Elf0, elephant=Elephant0, time=T, nodes=NodesIn, tpr=Tpr}=R) ->
     NextTpr = Tpr + pressure(G),
-    %%io:format("  move to ~p , pressure released = ~p~n", [To, pressure(G)]),
-    route(R#route{at=To, path=Path, time=T-1, tpr=NextTpr}).
+
+    {Elf,Nodes0} = update_actor(G, Elf0, NodesIn),
+    {Elephant, Nodes1} = update_actor(G, Elephant0, Nodes0),
+
+    route(R#route{elf=Elf, elephant=Elephant,
+                  time=T-1, tpr=NextTpr, nodes=Nodes1}).
+
 
 
 close_all_valves(G) ->
     [ digraph:add_vertex(G, V, (element(2,digraph:vertex(G, V)))#valve{open = false})
       || V <- digraph:vertices(G) ].
 
-execute_route(G, [FirstNode|Nodes]) ->
+execute_route(G, Time, Elf, Elephant, Nodes) ->
     close_all_valves(G),
-    [_|Path] = digraph:get_short_path(G, "AA", FirstNode),
-    %%io:format("Exec route: ~p~n", [[FirstNode|Nodes]]),
-    route(#route{g=G, path=Path, nodes=Nodes}).
-    %%io:format("   TPR -> ~p~n", [TPR]).
+    %% Add 1 to time, so actors take their first routes at that time
+    route(#route{g=G, time = Time+1, elf=Elf, elephant=Elephant, nodes=Nodes}).
 
 %% sample best route 1651
 %%execute_route(G, ["DD", "BB", "JJ", "HH", "EE", "CC"]).
 
-part1() ->
+work(Name) ->
     N = 8,
-    lists:foreach(fun(_) -> spawn(?MODULE, part1_worker, [self()]) end,
+    lists:foreach(fun(_) -> spawn(?MODULE, Name, [self()]) end,
                   lists:seq(1, N)),
     receive_result(0).
+
+part1() -> work(part1_worker).
+part2() -> work(part2_worker).
 
 receive_result(Max) ->
     receive
@@ -98,20 +99,29 @@ receive_result(Max) ->
             Max
     end.
 
-part1_worker(ResultPid) ->
+worker_with(ResultPid, MakeExecute) ->
     G = input(),
     OtherValves = [ V || V <- digraph:vertices(G),
                                   V =/= "AA" ] ,
-    OnlyUseful = lists:sort(fun(V1,V2) ->
+    OnlyUseful = shuffle(lists:sort(fun(V1,V2) ->
                                     {_, #valve{rate=R1}} = digraph:vertex(G,V1),
                                     {_, #valve{rate=R2}} = digraph:vertex(G,V2),
                                     R1 > R2
                             end,
                             (lists:filter(fun(V) ->
                                                   {_, #valve{rate = Rate}} = digraph:vertex(G, V),
-                                                  Rate > 0 end, OtherValves))),
-    io:format("length useful ~p~n", [length(OnlyUseful)]),
-    part1(ResultPid, G, OnlyUseful, execute_route(G, OnlyUseful)).
+                                                  Rate > 0 end, OtherValves)))),
+    solve(ResultPid, OnlyUseful, 0, MakeExecute(G)).
+
+part1_worker(ResultPid) ->
+    worker_with(ResultPid, fun(G) ->
+                                   fun(Route) -> execute_route(G, 30, #actor{}, undefined, Route) end
+                           end).
+
+part2_worker(ResultPid) ->
+    worker_with(ResultPid, fun(G) ->
+                                   fun(Route) -> execute_route(G, 26, #actor{}, #actor{}, Route) end
+                           end).
 
 shuffle([]) -> [];
 shuffle(List) ->
@@ -126,15 +136,14 @@ swap2(Valves) ->
     Val2 = element(P2, V),
     tuple_to_list(setelement(P1, setelement(P2, V, Val1), Val2)).
 
--spec part1(_, _, [], _) -> any().
-part1(_, _, [], TprMax) -> TprMax;
-part1(ResultPid, G, Valves, Tpr0) ->
+
+solve(ResultPid, Valves, Tpr0, Execute) ->
     Swapped = swap2(Valves),
-    SwappedTpr = execute_route(G, Swapped),
+    SwappedTpr = Execute(Swapped),
     if SwappedTpr > Tpr0 ->
             ResultPid ! SwappedTpr,
-            part1(ResultPid, G, Swapped, SwappedTpr);
-       true -> part1(ResultPid, G, Valves, Tpr0)
+            solve(ResultPid, Swapped, SwappedTpr, Execute);
+       true -> solve(ResultPid, Valves, Tpr0, Execute)
     end.
 
 
